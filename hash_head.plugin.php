e048ba3254894ff21f8cf8746aafd790829616af
mikelietz <codger+git@gmail.com>
2012-05-22 13:34:10 +0000

    Leverage new Version::get_git_short_hash() in Habari with API_VERSION 4957 or newer, from https://github.com/habari/system/commit/8ce3ec0b

--- hash_head.plugin.php
+++ hash_head.plugin.php
@@ -40,31 +40,7 @@ class HashHead extends Plugin {
 	 **/
 	private function make_hash_header()
 	{
-		return "<meta name='git_hash' content='" . self::get_git_short_hash() . "'>\n";
-	}
-
-	/**
-	 * Attempt to return the shortened git hash of the system source tree
-	 *
-	 * @return String The first 7 chars of the revision hash
-	 */
-	public static function get_git_short_hash()
-	{
-		$rev = '';
-		$ref_file = HABARI_PATH . Options::get( 'hashhead__path', '/system/' ) . '.git/HEAD';
-		if ( file_exists( $ref_file ) ) {
-			$info = file_get_contents( $ref_file );
-			// If the contents of this file start with "ref: ", it means we need to look where it tells us for the hash.
-			// CAVEAT: This is only really useful if the master branch is checked out
-			if ( strpos( $info, 'ref: ' ) === false ) {
-				$rev = substr( $info, 0, 7 );
-			}
-			else {
-				preg_match( '/ref: (.*)/', $info, $match );
-				$rev = substr( file_get_contents( HABARI_PATH . Options::get( 'hashhead__path', '/system/' ) . '.git/' . $match[1] ), 0, 7 );
-			}
-		}
-		return $rev;
+		return "<meta name='git_hash' content='" . Version::get_git_short_hash( HABARI_PATH . Options::get( 'hashhead__path', '/system' ) ) . "'>\n";
 	}
 }
 ?>
